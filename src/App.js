18f36ae9aa2c85a42230b6149be187e3f1681b02
Manoel Luis <manoelluisbusiness@gmail.com>
2022-06-24 17:29:38 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+//CSS
+import "./App.css";
+
+//React
+import { useCallback, useEffect, useState } from "react";
+
+// data
+import { wordsList } from "./data/words";
+
+// components
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //SELECIONA UMA CATEGORIA ALEATORIA
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+    //SELECIONA UMA PALAVRA ALEATORIA
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return { word, category };
+  };
+
+  //INICIA O JOGO
+  const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    console.log(word, category);
+
+    setGameStage(stages[1].name);
+  };
+
+  //PROCESSAR O LETTER INPUT
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  //REINICIA O JOGO
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Impressora"],
+  programação: ["Linguagem", "Framework", "Javascript", "React"],
+  alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo"],
+};
